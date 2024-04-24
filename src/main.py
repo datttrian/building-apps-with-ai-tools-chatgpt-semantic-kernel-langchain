f40a42403f0b9fa8f0d4937c97ac62d360d72bbb
Dat Tran <datttrian@gmail.com>
2024-04-24 05:30:22 +0000

    Learning to use Whisper for text to speech

--- src/main.py
+++ src/main.py
@@ -2,5 +2,9 @@ from pathlib import Path
 import os
 import openai
 from dotenv import load_dotenv
+
 load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv("OPENAI_API_KEY")
+with open(Path.cwd() / "src" / "lost_debit_card.wav", "rb") as audio_file:
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript)
