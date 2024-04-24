ba8097da87482b82bb67781c71ea0d31a71c1ae0
Dat Tran <datttrian@gmail.com>
2024-04-24 04:19:21 +0000

    Providing a discount through a UI

--- src/main.py
+++ src/main.py
@@ -1,32 +1,15 @@
-import os
-from dotenv import load_dotenv
-from openai import OpenAI
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI
+from starlette.responses import FileResponse
+from services.ai import generate_review
+app = FastAPI()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-load_dotenv()
 
-while True:
-    user_input = user_input = input(
-        "Enter a phrase and we’ll classify it as happy or sad\n"
-    )
-    if user_input == "quit" or user_input == "exit":
-        break
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": (
-                    "You are a sentiment classification bot, print"
-                    "out if the user is happy or sad. Only print"
-                    "out happy or sad."
-                ),
-            },
-            {"role": "user", "content": user_input},
-        ],
-        temperature=0.7,
-        max_tokens=150,
-    )
+@app.get("/")
+async def read_index():
+    return FileResponse('static/index.html')
 
-    response_message = response.choices[0].message
-    print(response_message)
+
+@app.get("/api/gpt")
+async def gpt(review: str):
+    return generate_review(review)
