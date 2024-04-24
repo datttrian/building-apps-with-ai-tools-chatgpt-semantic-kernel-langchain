32aace97b9044616f86971f73de5722ae8d7a9f9
Dat Tran <datttrian@gmail.com>
2024-04-24 19:21:01 +0000

    Generative AI–powered tests

--- src/main.py
+++ src/main.py
@@ -1,38 +1,66 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.prompt import PromptTemplate
-from langchain.output_parsers import PydanticOutputParser
-from pydantic import BaseModel, Field
-
-
-class Person(BaseModel):
-    first_name: str = Field(description="first name")
-    last_name: str = Field(description="last name")
-    dob: str = Field(description="date of birth")
-
-
-class PeopleList(BaseModel):
-    people: list[Person] = Field(description="A list of people")
-
-
-model = ChatOpenAI(model="gpt-3.5-turbo")
-people_data = model.predict(
-    "Generate a list of 10 fake peoples information. Only return the list. Each person should have a first name, last name and date of birth."
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
 )
+from langchain.chains import LLMChain
+from langchain.schema import BaseOutputParser
 
-parser = PydanticOutputParser(pydantic_object=PeopleList)
 
-prompt = PromptTemplate(
-    template="Answer the user query.\n{format_instructions}\n{query}\n",
-    input_variables=["query"],
-    partial_variables={
-        "format_instructions": parser.get_format_instructions()
-    },
-)
+def generate_travel_recommendations(travel_requests):
+    """
+    Generate travel recommendations based on user requests
+    """
+    # create templates
+    system_template_travel_agent = """You are travel recommendation agent. Provide a short recommendation based on the user request."""
+    system_message_prompt = SystemMessagePromptTemplate.from_template(
+        system_template_travel_agent
+    )
 
-_input = prompt.format_prompt(query=people_data)
+    human_template_travel_agent = "{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(
+        human_template_travel_agent
+    )
 
-model = ChatOpenAI()
-output = model.predict(_input.to_string())
+    # create full prompt
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
 
-parsed = parser.parse(output)
-print(parsed)
+    chain = LLMChain(llm=ChatOpenAI(temperature=1), prompt=chat_prompt)
+
+    recommendations = []
+    for travel_request in travel_requests:
+        recommendations.append(chain.run(travel_request))
+
+    return recommendations
+
+
+def generate_travel_requests(n=5) -> list[str]:
+    """Generate travel requests
+    n: number of requests
+    """
+    # TODO 1 add prompt
+    system_template_travel_agent = """"""
+    system_message_prompt = SystemMessagePromptTemplate.from_template(
+        system_template_travel_agent
+    )
+
+    # create full prompt
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt])
+
+    chain = LLMChain(llm=ChatOpenAI(model="gpt-4"), prompt=chat_prompt)
+    # TODO 2 return results
+    return results
+
+
+# generate some requests
+# travel_requests = generate_travel_requests()
+travel_requests = ["I want a beach vacation"]
+# print(travel_requests)
+# get the recommendations
+
+
+recommendations = generate_travel_recommendations(travel_requests)
+print(recommendations)
