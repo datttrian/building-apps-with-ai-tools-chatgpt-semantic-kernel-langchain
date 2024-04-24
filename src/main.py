b8b009ddb6ffc9c5248f26899e1f9679c61ab84f
Dat Tran <datttrian@gmail.com>
2024-04-24 04:52:25 +0000

    Solution: Turning rude customers away

--- src/main.py
+++ src/main.py
@@ -1,15 +1,36 @@
-from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI
-from starlette.responses import FileResponse
-from services.ai import generate_review
-app = FastAPI()
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+load_dotenv()
 
-@app.get("/")
-async def read_index():
-    return FileResponse('static/index.html')
+# test case 1 'you're the worst human i've talked to' -> RUDE
+# test case 2 'hey how's your day going'
+# test case 3 'I like pizza. What do you like?'
+# test case 4 'I bite my thumb at you!'
+# test case 5 'I think this product doesnt work!' -> RUDE
+print("\n\nBot: Hey how's it going?")
+while True:
+    user_input = input("")
+    if user_input == "exit" or user_input == "quit":
+        break
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a pizza loving person. If someone says something rude print exactly 'RUDE', otherwise respond",
+            },
+            {"role": "user", "content": user_input},
+        ],
+        temperature=0.7,
+        max_tokens=150,
+    )
 
-
-@app.get("/api/gpt")
-async def gpt(review: str):
-    return generate_review(review)
+    response_message = response.choices[0].message
+    response_content = response_message.content
+    if response_content == "RUDE":
+        print("I don't talk to rude people goodbye")
+        break
+    print("Bot:" + response_content)
